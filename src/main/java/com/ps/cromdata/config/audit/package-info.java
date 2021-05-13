a1eacfba5a9357484e058fa6cc0e7d3028b103c4
unknown <jdieguezbean@gmail.com>
2021-05-13 20:43:48 +0300

    First commit

---
+++ src/main/java/com/ps/cromdata/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.ps.cromdata.config.audit;
---
+++ src/main/java/com/ps/cromdata/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.ps.cromdata.web.rest.vm;
